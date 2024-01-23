18efb64e553518525a8b5f7e6f15e6a23b7c4c29
s00222321 <s00222321@atu.ie>
2024-01-23 12:41:53 +0000

    quick change

--- src/app/basics/compute.spec.ts
+++ src/app/basics/compute.spec.ts
@@ -1,6 +1,10 @@
 import {compute} from './compute';
 
 describe ('compute', () => {
+    it('Ellen Woodward - S00222321', () => {
+        const result = compute(-1);
+        expect(result).toBe(0);
+    });
     it('should return 0 if the input is negative', () => {
         const result = compute(-1);
         expect(result).toBe(0);
