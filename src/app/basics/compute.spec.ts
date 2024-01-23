af1102c01a987b3ea02a950881197ecc02876dfd
s00222321 <s00222321@atu.ie>
2024-01-23 12:39:48 +0000

    added screenshot

--- src/app/basics/compute.spec.ts
+++ src/app/basics/compute.spec.ts
@@ -5,8 +5,20 @@ describe ('compute', () => {
         const result = compute(-1);
         expect(result).toBe(0);
     });
+    it('should not return 0 if the input is negative', () => {
+        const result = compute(1);
+        expect(result).not.toBe(0);
+    });
     it('should return input+1 if the input is positive', () => {
         const result = compute(1);
         expect(result).toBe(2);
+    });
+    it('should return greater than input if input is positive', () => {
+        const result = compute(1);
+        expect(result).toBeGreaterThan(1);
+    });
+    it('should return less than input+2 if input is positive', () => {
+        const result = compute(1);
+        expect(result).toBeLessThan(3);
     });
 })
